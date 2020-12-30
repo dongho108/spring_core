434c61bd46c568c14a555bd4809c5fa04c11c48e
dongho <dongho108@naver.com>
2020-12-30 14:56:25 +0900

    Initial commit

---
+++ src/main/java/hello/core/member/Grade.java
@@ -0,0 +1,7 @@
+package hello.core.member;
+
+public enum Grade {
+    //회원 등급 2가지 일반, vip
+    BASIC,
+    VIP
+}
---
+++ src/main/java/hello/core/member/MemberServiceImlp.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+public class MemberServiceImlp implements MemberService{
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImlp(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById((memberId));
+    }
+
+}
