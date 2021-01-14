4c2af6f7f3b827d3b05365705caafb093997bef1
dongho <dongho108@naver.com>
2021-01-14 18:50:27 +0900

    add Bean Life Cycle, CallBack

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,32 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetwordClient client = ac.getBean(NetwordClient.class);
+
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+
+        @Bean
+        public NetwordClient networdClient() {
+            NetwordClient networdClient = new NetwordClient();
+            networdClient.setUrl("http://hello-spring.dev");
+            return networdClient;
+        }
+    }
+
+}
